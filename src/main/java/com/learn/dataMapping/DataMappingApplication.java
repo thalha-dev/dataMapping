d7aa719681ec23c6d195d7bc213524e904c1cd4b
thalha-dev <dev.thalha@gmail.com>
2024-02-17 12:30:56 +0530

    initial files

---
+++ src/main/java/com/learn/dataMapping/DataMappingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.dataMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMappingApplication.class, args);
+	}
+
+}
